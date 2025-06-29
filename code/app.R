71a5734587218035c9164db0f1de314ff38f790c
PaulineDataWard <polo@paulineward.me.uk>
2025-06-29 21:31:22 +0100

    try to link up outputs to reactive for selection of one rain station

--- code/app.R
+++ code/app.R
@@ -11,9 +11,6 @@ rainfall_data <- read_csv("../data/rainfall/aggreg_edinburgh_rainfall.csv")
 rainfall_data <-   rainfall_data |>
   mutate(MeasurementDate = as.POSIXct(Timestamp, format = "%d/%m/%Y")) 
 
-# Allows the ui to draw the input options from the rainfall data eg col names
-#dataset <- rainfall_data
-
 rain_stations <- unique(rainfall_data$rain_station)
 
 # TBC https://mastering-shiny.org/basic-app.html#server-function terrible examples as usual
@@ -24,10 +21,13 @@ ui <- fluidPage(
   titlePanel("Edinburgh Rainfall by D4CAE"),
   
   sidebarPanel(
-    selectInput(inputId = "chosen_rain_stations", 
+    selectInput(inputId = "chosen_rain_station", 
                 label = "Optionally, select a rainfall station", 
                 choices = c(None=',', rain_stations)),
-    sliderInput('rainfall_in_mm', 'Amount of rain', min=0, max=nrow(rainfall_data),
+    
+    sliderInput(inputId = 'rainfall_in_mm', 
+                label = 'Amount of rain', 
+                min=0, max=nrow(rainfall_data),
                 value=min(0, nrow(rainfall_data)), step=0.5, round=0),
     
     dateRangeInput(inputId = "myDateRange", label = "Dates for rain, walking in the rain"),
@@ -35,51 +35,46 @@ ui <- fluidPage(
   ),
   
   mainPanel(
-    verbatimTextOutput("nice_summary"),
+    verbatimTextOutput(outputId = "nice_summary"),
     
-    plotOutput('rainplot'),
+    plotOutput(outputId = 'rainplot'),
     
-    tableOutput("fable_table")
+    tableOutput(outputId = "fable_table")
   )
 ) 
 
 # Server - Process the data
 server <- function(input, output, session){
-  
+
+  drizzly_dataset <- reactive({
+    
+    rainfall_data_to_plot <- rainfall_data |> 
+      filter(str_detect(rain_station, get(input$chosen_rain_station)))
+    
+  })
+    
   output$nice_summary <- renderPrint({
-    # not working
-   # selected_stn <- get(input$chosen_rain_stations)
     
     # For now, just narrow down with hard-wired choice, 'Mint'
     the_relevant_data <- rainfall_data |>
-      filter(str_detect(rain_station, "Murray")) 
+      filter(str_detect(rain_station, get(input$chosen_rain_station))) 
     
     summary(the_relevant_data)
   })
   
 
-  
-  # pluie_dataset <- reactive({
-  #   
-  #   # did the plot first, in wee bit rain script, then put here 
-  #   rainfall_data |> ggplot(aes(x = MeasurementDate, y = rainfall_in_mm))+
-  #     geom_line()
-  #   
-  # })
   output$rainplot <- renderPlot({
-    rainfall_data |> ggplot(aes(x = MeasurementDate, y = rainfall_in_mm))+
+    rainfall_data_to_plot |> ggplot(aes(x = MeasurementDate, y = rainfall_in_mm))+
       geom_line()
     })
   
 
   output$fable_table <- renderTable({
-    # not working? 
-    #dataset_pour_la_table <- get(input$chosen_rain_stations)
-    #dataset_pour_la_table
+    rainfall_data_to_plot
     
     # workaround, just display narrowed down table
-    rainfall_data |>
-      filter(str_detect(rain_station, "Murray"))
+    #rainfall_data |>
+     # filter(str_detect(rain_station, "Murray"))
   })
   }
 
