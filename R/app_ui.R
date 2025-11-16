af757f48c2f4d3e8a7a2a8ec81ad808a88ae1feb
PaulineDataWard <polo@paulineward.me.uk>
2025-11-16 20:19:22 +0000

    formatting - h1 for accessibility

--- R/app_ui.R
+++ R/app_ui.R
@@ -10,8 +10,9 @@ app_ui <- function(request) {
     golem_add_external_resources(),
     # Your application UI logic
     fluidPage(
-      tags$img(src = "www/logo_organization_trees_bees_PCs_peeps.png"),
-      "Data 4 Climate Action Edinburgh",
+      tags$img(src = "www/logo_organization_trees_bees_PCs_peeps.png",
+               href="https://data-4-climate-action-edinburgh.github.io/home/"),
+      h1("Data 4 Climate Action Edinburgh"), # h1 formatting is important for accessibility
       tabsetPanel(
         tabPanel("Rainfall",
                  mod_second_rainfall_ui("secondmod")
